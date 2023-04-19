d2792d9807664f9b80a4a66e78cee9f0a2b9fc62
SeikohDash <e.wicht@ekml.ch>
2023-04-19 14:49:17 +0200

    Test

--- APP_FILMS_164/__init__.py
+++ APP_FILMS_164/__init__.py
@@ -44,11 +44,10 @@ try:
     from APP_FILMS_164.genres import gestion_genres_crud
     from APP_FILMS_164.demos_om_164 import routes_demos
 
-    from APP_FILMS_164.films_genres import gestion_films_genres_crud
     from APP_FILMS_164.erreurs import msg_avertissements
 
-    from APP_FILMS_164.clients import gestion_films_crud
-    from APP_FILMS_164.clients import gestion_films_wtf_forms
+    from APP_FILMS_164.clients import gestion_clients_crud
+    from APP_FILMS_164.clients import gestion_clients_wtf_forms
 
     from APP_FILMS_164.assurance import gestion_assu_crud
     from APP_FILMS_164.assurance import gestion_assu_wtf_forms
